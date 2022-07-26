9826257edfbbb1f30d0bb4509e7b0dc26f0af373
Iilith <74946934+Iilith@users.noreply.github.com>
2022-07-26 18:16:05 -0300

    Create Retangulo.java

---
+++ Retangulo.java
@@ -0,0 +1,26 @@
+public class Retangulo implements AreaCalculavel{
+	private double largura;
+	private double altura;
+	
+	public double calcularArea() {
+		return largura * altura;
+	}
+
+	public double getLargura() {
+		return largura;
+	}
+
+	public void setLargura(double largura) {
+		this.largura = largura;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
